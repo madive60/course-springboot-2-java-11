18f506ff9c2a1a3db4dfe088fefb1391c3fc6834
madive60 <madive60@gmail.com>
2021-04-09 18:09:15 -0300

    User insert

--- src/main/java/com/madive60/course/resources/UserResource.java
+++ src/main/java/com/madive60/course/resources/UserResource.java
@@ -1,16 +1,21 @@
 package com.madive60.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.madive60.course.entities.User;
 import com.madive60.course.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,4 +36,11 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
